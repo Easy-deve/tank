441f722f09bc83e00445e1bc5535cc499de80aed
Yang zhiyao <yangzhiyao@zhigui.com>
2021-08-17 22:55:29 +0800

    Fix: add test junit and test image io reading

---
+++ src/test/ImageTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Author: yangzhiyao
+ * @DateTime: 2021/8/16 23:49
+ */
+public class ImageTest {
+    @Test
+    public void test() {
+        try {
+//            BufferedImage bufferedImage = ImageIO.read(new File("D:/test/image/hhh.jpg"));
+//            Assert.assertNotNull(bufferedImage);
+            BufferedImage buff = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/hhh.jpg"));
+            Assert.assertNotNull(buff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
