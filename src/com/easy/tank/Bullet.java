f1729b6e73cd876414ad861723fea4b947489222
Yang zhiyao <yangzhiyao@zhigui.com>
2021-08-18 08:40:11 +0800

    Fix: replace image for bullet

--- src/com/easy/tank/Bullet.java
+++ src/com/easy/tank/Bullet.java
@@ -25,10 +25,27 @@ public class Bullet {
         if (!live) {
             tankFrame.bulletList.remove(this);
         }
-        Color color = g.getColor();
-        g.setColor(Color.RED);
-        g.fillOval(x, y, WIDTH, HEIGHT);
-        g.setColor(color);
+//        Color color = g.getColor();
+//        g.setColor(Color.RED);
+//        g.fillOval(x, y, WIDTH, HEIGHT);
+//        g.setColor(color);
+
+        switch (bulletDir) {
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL, x, y, null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.bulletU, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD, x, y, null);
+                break;
+            default: break;
+        }
+
         move();
     }
 
